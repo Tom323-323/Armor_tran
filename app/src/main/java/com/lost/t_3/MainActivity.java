785b5330e91638401bbb1f77d562ec1f01daf47f
Tom323-323 <64865370+Tom323-323@users.noreply.github.com>
2022-01-21 14:28:01 +0300

    21.01.2022 - xml adaptation. System Ui Flag layout.

--- app/src/main/java/com/lost/t_3/MainActivity.java
+++ app/src/main/java/com/lost/t_3/MainActivity.java
@@ -17,6 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Скрыть нижную панель навигации_________________
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
         init();
     }
 
